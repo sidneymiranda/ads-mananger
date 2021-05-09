75592b391222349357572fbd1a4a185641be8da4
Sidney Miranda <sidneysmiranda@hotmail.com>
2021-05-09 13:51:54 -0300

    Criada estrutura do projeto

---
+++ src/main/java/br/com/capgemini/desafio/adsmanager/AdsManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.capgemini.desafio.adsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/capgemini/desafio/adsmanager/AdsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.capgemini.desafio.adsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
